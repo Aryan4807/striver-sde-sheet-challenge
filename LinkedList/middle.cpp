0af42ad1672089a3af496dc5bdce3339bf2f2311
Aryan4807 <65225779+Aryan4807@users.noreply.github.com>
2026-08-07 22:20:48 +0530

    day 1 to the new beginning of end

---
+++ LinkedList/middle.cpp
@@ -0,0 +1,9 @@
+ ListNode* middleNode(ListNode* head) { // For a mutable list
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != nullptr && fast->next != nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        return slow;
+    }
