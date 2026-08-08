bd4580f5808671dda03d33eb32292599d559cb44
Aryan4807 <65225779+Aryan4807@users.noreply.github.com>
2026-08-09 02:45:56 +0530

    kadane day 2

---
+++ Array 1/day1/kadane.cpp
@@ -0,0 +1,12 @@
+ #include <vector>
+#include <algorithm>
+ int maxSubArray(std::vector<int>& nums) {
+        int i, j=0,n=nums.size(),c=nums[0], maxsub=nums[0];
+        for(i=0;i<n;i++){
+            j=j+nums[i];
+            if(nums[i]>j)j=nums[i];
+            c=std::max(c,j);
+            maxsub=max(maxsub,c);
+        }
+        return maxsub;
+    }
